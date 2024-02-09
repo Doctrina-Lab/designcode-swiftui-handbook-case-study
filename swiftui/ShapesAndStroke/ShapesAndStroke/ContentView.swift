b560dbed73d909907f7b716de8fc721ea6412caf
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-02-09 16:40:17 +0200

    Add ShapesAndStroke sample

---
+++ swiftui/ShapesAndStroke/ShapesAndStroke/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShapesAndStroke
+//
+//  Created by Volodymyr Myroniuk on 09.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftui/ShapesAndStroke/ShapesAndStroke/ShapesAndStrokeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapesAndStrokeApp.swift
+//  ShapesAndStroke
+//
+//  Created by Volodymyr Myroniuk on 09.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ShapesAndStrokeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
