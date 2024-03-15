06038a2dc8ee03cb9debf7e3fe79f30a9023e7b9
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-03-15 14:40:14 +0200

    Add AnimationTransformations

---
+++ TransformAnimations/TransformAnimations/TransformAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransformAnimationsApp.swift
+//  TransformAnimations
+//
+//  Created by Meng To on 2021-01-28.
+//
+
+import SwiftUI
+
+@main
+struct TransformAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
