42b5dcbb6f60dd02bf2a50976fe6b4db73b811e0
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-03-15 14:32:15 +0200

    Add Mask

---
+++ Mask/Mask/MaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaskApp.swift
+//  Mask
+//
+//  Created by Meng To on 2021-02-09.
+//
+
+import SwiftUI
+
+@main
+struct MaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
