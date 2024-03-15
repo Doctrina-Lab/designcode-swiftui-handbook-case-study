cc7fb4294dc2366f2c950ecc109da6205f784dec
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-03-15 14:41:02 +0200

    Add TapAnimationDelay

---
+++ TapAnimationDelay/TapAnimationDelay/TapAnimationDelayApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapAnimationDelayApp.swift
+//  TapAnimationDelay
+//
+//  Created by Meng To on 2021-02-03.
+//
+
+import SwiftUI
+
+@main
+struct TapAnimationDelayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
