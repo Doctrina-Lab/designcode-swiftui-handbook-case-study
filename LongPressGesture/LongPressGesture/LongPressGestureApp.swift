261f3fe7539a8695ab734b7ae05200b917fc5bc4
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-03-15 14:41:52 +0200

    Add LongPressGesture

---
+++ LongPressGesture/LongPressGesture/LongPressGestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  LongPressGestureApp.swift
+//  LongPressGesture
+//
+//  Created by Meng To on 2021-02-02.
+//
+
+import SwiftUI
+
+@main
+struct LongPressGestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
